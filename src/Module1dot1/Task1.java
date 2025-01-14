d1a6a40510b54f76d3b49f10a02f5b603e14cfb4
veikk <valttejo@metropolia.fi>
2025-01-14 09:54:16 +0200

    Bello

--- src/Module1dot1/Task1.java
+++ src/Module1dot1/Task1.java
@@ -2,6 +2,6 @@ package Module1dot1;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello fellow user!");
     }
 }

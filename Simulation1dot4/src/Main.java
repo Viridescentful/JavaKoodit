d9a85bd1c652cc985eb9d3b22d50472508bc59b1
veikk <musicaldominator@gmail.com>
2025-02-06 02:24:06 +0200

    bello

---
+++ Simulation1dot4/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    /*
+    Task 1, 2, 4, 5?
+
+    Question is extremely unclear?
+
+    If we were to discuss how the learned material is present in a bank system. We can start from how the functions are seperated;
+    into sections and when each function is gonna be needed to call. In the B events, the bank would finish up servering the customers or receive a customer;
+    Once we exit phase B, we would start the serving activity in phase C in all operators that are currently handling a customer.
+
+    The serve times and arrival times wouldn't be fixed, as the arrival of a customer and the time it takes to serve them varies, which create variability in the distrobution;
+
+    Considering we are just learning multithreading, this would also be seen as multiple servicepoints operating without needing a timeslice system;
+
+    Now if this was applied as a simulation, like the metrostation ticket system. We would first initialise the whole system and prepare the B events for start up;
+    In phase B, the metro station would receive users into a queue, which will then exit into the ticket queue on the next interval. In Phase B, the user would also;
+    leave the ticket booth for the corresponding dock for the metro, which is also a queue as the metro isn't always available. In Phase C, everyone in hte ticket booth;
+    would start to be served, and everyone inside the metro would be leaving. Once we return back to phase B, the people from the booth would leave and people from the metro;
+    would have already stepped off and the metro returned to the station/a new one has arrived;
+
+    Task 3: Kuva
+
+     */
+}
